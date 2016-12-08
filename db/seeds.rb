77c4d5792889e2148bb0a5adca8dd5ff82636499
Peter Jang <peter.x.jang@gmail.com>
2016-12-07 18:35:47 -0600

    Add seed file for fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+names = ['test', 'bob', 'jane', 'lisa']
+
+names.each do |name|
+  user = User.new(name: name, email: "#{name}@gmail.com", password: "password")
+  user.save
+  3.times do
+    article = Article.new(
+      title: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraph(10),
+      url: Faker::Internet.url,
+      user_id: user.id
+    )
+    article.save
+  end
+end
+
+users = User.all
+articles = Article.all
+
+articles.each do |article|
+  users.each do |user|
+    vote = Vote.new(
+      user_id: user.id,
+      article_id: article.id,
+      vote_value: [-1, 0, 1].sample
+    )
+    vote.save
+  end
+end
+
+puts "Done!"
